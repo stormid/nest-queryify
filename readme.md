1bd949d4ebab86baf37987ad910aaa81f6fe026a
Phil Oyston <oystonpj@oystec.co.uk>
2015-08-31 16:09:47 +0100

    adds nuspec file

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Nest Queryify
+Using a query pattern to access data within Elasticsearch via NEST
+
+### Build Status
+[![Build status](https://ci.appveyor.com/api/projects/status/qdwdh7gs1f14jvoi/branch/master?svg=true)](https://ci.appveyor.com/project/Philo/nest-queryify/branch/master)
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,17 +1,19 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Nest.Queryify")]
 [assembly: AssemblyDescription("Provides a mechanism to interact with Elasticsearch via a query object pattern")]
 [assembly: AssemblyCompany("Storm ID Ltd")]
-[assembly: AssemblyProduct("Nest.Queryify")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 
-[assembly: ComVisible(false)]
+#if DEBUG
+    [assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyProduct("Nest.Queryify")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
